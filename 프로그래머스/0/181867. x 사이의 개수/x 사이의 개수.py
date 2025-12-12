02476ce22269c9151f7ca66f0dde6d72312b66e6
thbins <joons0218@gmail.com>
2025-12-12 20:14:38 +0900

    len(part)를 이용한 풀이

--- 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
+++ 프로그래머스/0/181867. x 사이의 개수/x 사이의 개수.py
@@ -1,12 +1,2 @@
 def solution(myString):
-    answer = []
-    count = 0
-    l = list(myString.split("x"))
-    
-    for i in l:
-        count = 0
-        for _ in i:
-            count += 1
-        answer.append(count)
-        
-    return answer
+    return [len(part) for part in myString.split("x")]
