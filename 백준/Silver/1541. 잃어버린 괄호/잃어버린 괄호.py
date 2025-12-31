1b948f4caa64f18c78c273c6138ea7fc7d75ec2a
Taeho Kim <joons0218@gmail.com>
2026-01-01 05:35:12 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,12 @@
+import sys
+
+expr = sys.stdin.readline().strip()
+
+parts = expr.split('-')
+
+result = sum(map(int, parts[0].split('+')))
+
+for part in parts[1:]:
+    result -= sum(map(int, part.split('+')))
+
+print(result)
