7aa827f0df53bfd3735bcc3d3e814069759338bf
Taeho Kim <joons0218@gmail.com>
2026-01-25 22:17:32 +0900

    [level 0] Title: 분수의 덧셈, Time: 0.00 ms, Memory: 9.19 MB -BaekjoonHub

---
+++ 프로그래머스/0/120808. 분수의 덧셈/분수의 덧셈.py
@@ -0,0 +1,10 @@
+def gcd(a, b):
+    while b:
+        a, b = b, a % b
+    return a
+
+def solution(numer1, denom1, numer2, denom2):
+    numer = numer1 * denom2 + numer2 * denom1
+    denom = denom1 * denom2
+    g = gcd(numer, denom)
+    return [numer // g, denom // g]
