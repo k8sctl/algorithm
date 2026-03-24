82844d841cca5c12ddff3ba6961a4631d6b57e93
Taeho Kim <joons0218@gmail.com>
2026-03-24 22:40:37 +0900

    [Bronze I] Title: 평균, Time: 208 ms, Memory: 18768 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,38 +1,26 @@
-import java.io.*;
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
-    static int findMax(int[] array) {
-        int max = array[0];
-        for (int n : array) {
-            if (n > max) {
-                max = n;
+	
+    public static void main(String[] args) {
+    	Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        int[] scores = new int[N];
+        
+      	for (int i = 0 ; i < N ; i++) {
+        	scores[i] = sc.nextInt();
+        }
+        
+        int sum = 0;
+        int maxScore = scores[0];
+        
+        for (int score : scores) {
+        	if (score > maxScore) {
+            	maxScore = score;
             }
+    		sum += score;
         }
-        return max;
-    }
-
-    static double newScoreSum(int[] array) {
-        double sum = 0;
-        int max = findMax(array);
-
-        for (int n : array) {
-            sum += (double) n / max * 100;
-        }
-        return sum;
-    }
-
-    public static void main(String[] args) throws Exception {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-
-        int[] array = new int[N];
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        for (int i = 0; i < N; i++) {
-            array[i] = Integer.parseInt(st.nextToken());
-        }
-
-        System.out.println(newScoreSum(array) / N);
+        System.out.print(sum * 100.0 / maxScore / N);
     }
 }
