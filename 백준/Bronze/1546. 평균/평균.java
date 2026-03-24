df87ef940f184b6a1245c5133670d3f4cd747031
Taeho Kim <joons0218@gmail.com>
2026-03-24 22:44:14 +0900

    [Bronze I] Title: 평균, Time: 108 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,25 +1,26 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	
-    public static void main(String[] args) {
-    	Scanner sc = new Scanner(System.in);
-        
-        int N = sc.nextInt();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
         int[] scores = new int[N];
-        
-      	for (int i = 0 ; i < N ; i++) {
-        	scores[i] = sc.nextInt();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            scores[i] = Integer.parseInt(st.nextToken());
         }
-        
+
         int sum = 0;
         int maxScore = scores[0];
-        
+
         for (int score : scores) {
-        	if (score > maxScore) {
-            	maxScore = score;
+            if (score > maxScore) {
+                maxScore = score;
             }
-    		sum += score;
+            sum += score;
         }
         System.out.print(sum * 100.0 / maxScore / N);
     }
