3303f3fca44b1fbee17a0ca8e27e4a20fb9270a5
Taeho Kim <joons0218@gmail.com>
2026-06-13 23:31:00 +0900

    [level 0] Title: 순서쌍의 개수, Time: 0.05 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int count = 0;
+        for (int i = 1; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                count += 2;         
+                if (i == n / i) count--;
+            }
+        }
+        return count;
+    }
+}
